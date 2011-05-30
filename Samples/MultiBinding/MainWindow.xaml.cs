74b39db081337c0c1617c4687e724b574a4b2020
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-05-30 03:37:59 +0100

    MultiBinding sample project added

---
+++ Samples/MultiBinding/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CodeBinding;
+
+namespace MultiBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BindingEx.FromExpression(() => result.Text == (1000 * int.Parse(arg1.Text) + 100 * int.Parse(arg2.Text) + 10 * int.Parse(arg3.Text) + int.Parse(arg4.Text)).ToString());
+        }
+    }
+}
