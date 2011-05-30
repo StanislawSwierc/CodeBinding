c5c1417bf1bb4dc38baf295d5dcd03e2d95dc7c9
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-05-30 02:11:21 +0100

    SingleBinding sample project added

---
+++ Samples/SingleBinding/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CodeBinding;
+
+namespace SingleBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Expression has to have \"<target object expression>.<target property> == <expr>\" structure
+            BindingEx.FromExpression(() => textBox.FontSize == slider.Value);
+        }
+    }
+}
