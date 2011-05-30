ffb0ce6701c48f22c0f284c88071a9dcf5cc36a0
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-05-30 02:27:20 +0100

    Excesive information removed from AssemblyInfo files.

--- CodeBinding.Rx/Properties/AssemblyInfo.cs
+++ CodeBinding.Rx/Properties/AssemblyInfo.cs
@@ -1,26 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeBinding.Rx")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("CodeBinding.Rx")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("799ca1f5-8a1b-4763-89f9-e703b92b1a2b")]
 
 // Version information for an assembly consists of the following four values:
 //
--- CodeBinding/Properties/AssemblyInfo.cs
+++ CodeBinding/Properties/AssemblyInfo.cs
@@ -1,26 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeBinding")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("CodeBinding")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f3aea095-149c-40a4-8584-d3e22e004d4b")]
 
 // Version information for an assembly consists of the following four values:
 //
