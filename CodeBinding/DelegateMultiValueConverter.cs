e9e926508eb1e4dc555ba9facc99146a595deb6a
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-05-31 00:08:39 +0100

    DelegateValueConverter and DelegateMultiValueConverter extracted from BindingEx.cs to their own files

---
+++ CodeBinding/DelegateMultiValueConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+using System.Windows.Data;
+
+namespace CodeBinding
+{
+    public class DelegateMultiValueConverter : IMultiValueConverter
+    {
+        private Delegate m_Delegate;
+        private Type[] m_ParametersType;
+        private object[] m_ParametersDefaultValues;
+
+        public DelegateMultiValueConverter(Delegate converter)
+        {
+            m_Delegate = converter;
+            m_ParametersType = m_Delegate.Method.GetParameters().Skip(1).Select(p => p.ParameterType).ToArray();
+        }
+
+        private void CreateParameterDefaultValues()
+        {
+            m_ParametersDefaultValues = new object[m_ParametersType.Length];
+            for (int i = 0; i < m_ParametersType.Length; i++)
+            {
+                m_ParametersDefaultValues[i] = m_ParametersType[i].IsValueType ? Activator.CreateInstance(m_ParametersType[i]) : null;
+            }
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object result = null;
+            if (targetType != m_Delegate.Method.ReturnType)
+            {
+                throw new ArgumentException("targetType");
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == System.Windows.DependencyProperty.UnsetValue)
+                {
+                    // Create default values first time they are needed
+                    if (m_ParametersDefaultValues == null) CreateParameterDefaultValues();
+                    values[i] = m_ParametersDefaultValues[i];
+                }
+            }
+
+            try
+            {
+                result = m_Delegate.DynamicInvoke(values);
+            }
+            catch (TargetInvocationException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                result = System.Windows.DependencyProperty.UnsetValue;
+            }
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeBinding/DelegateValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace CodeBinding
+{
+    public class DelegateValueConverter : IValueConverter
+    {
+        private Delegate m_Delegate;
+
+        public DelegateValueConverter(Delegate converter)
+        {
+            m_Delegate = converter;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != m_Delegate.Method.ReturnType)
+            {
+                throw new ArgumentException("targetType");
+            }
+            return m_Delegate.DynamicInvoke(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
