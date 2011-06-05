2be049bbed9be0cc274f3ac8e9b0fe249228d240
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-06-05 14:36:21 +0100

    ObservableEx.FromExpression returns and IObservable<T> which creates new ObservableBindingTarget<T> on subscription. This is first step to implement error propagation logic in case Expression cannot be evaluated.

--- CodeBinding.Rx/ObservableEx.cs
+++ CodeBinding.Rx/ObservableEx.cs
@@ -21,25 +21,26 @@ namespace CodeBinding.Rx
         /// <returns></returns>
         // TODO: Change return type back to IObserbable<TResult>
         //       This will require client to save reference of the returned object
-        public static ObservableBindingTarget<TResult> FromExpression<TResult>(Expression<Func<TResult>> expression)
+        public static IObservable<TResult> FromExpression<TResult>(Expression<Func<TResult>> expression)
         {
             Contract.Requires(expression != null);
             Contract.Ensures(Contract.Result<IObservable<TResult>>() != null);
 
-            // Create target object
-            ObservableBindingTarget<TResult> target = new ObservableBindingTarget<TResult>();
-
-            // Create expression which can be used to create bingind
-            // () => target.Value == <expression.Body>
-            Expression<Func<bool>> expr = Expression.Lambda<Func<bool>>(
-                Expression.Equal(
-                    Expression.Property(Expression.Constant(target), "Value"),
-                    expression.Body));
             
             // Create binding
-            BindingEx.BindFromExpression(expr);
+            var binding = BindingEx.FromExpression(expression);
 
-            return target;
+            return Observable.Create<TResult>(observer =>
+                {
+                    // Create target object
+                    var target = new ObservableBindingTarget<TResult>();
+                    target.SetBinding(ObservableBindingTarget<TResult>.ValueProperty, binding);
+                    return new CompositeDisposable(
+                        target.Subscribe(observer),
+                        target
+                        );
+                }
+            );
         }
     }
 }
